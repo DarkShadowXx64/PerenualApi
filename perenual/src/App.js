57d0676287278c1eae0346d929b318d06343ee8c
DarkShadowXx64 <gabo.garcia.osorio@gmail.com>
2023-10-20 10:55:05 -0500

    api

--- perenual/src/App.js
+++ perenual/src/App.js
@@ -1,55 +1,89 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
-function Plants() {
-    const [data, setData] = useState(null);
-    const [plant, setPlant] = useState('');
+function Plant() {
+    const [pestData, setPestData] = useState({});
+    const [obtainingData, setObtainingData] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
 
-    const handleInputChange = (e) => {
-        setPlant(e.target.value);
-    }
+    const getPestInfo = async () => {
+        setObtainingData(true);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                if (plant) {
-                    const response = await axios.get(`https://perenual.com/api/pest-disease-list?key=sk-ukQs6532880c5fbdb2577&q=${plant}`);
-                    setData(response.data);
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
+        try {
+            const API_KEY = 'sk-DIAs6532876b0072f2567'; // Tu clave API
+            const response = await axios.get(`https://perenual.com/api/pest-disease-list?key=${API_KEY}&q=${searchTerm}`);
+
+            if (response.status !== 200) {
+                console.log(response);
+                return Swal.fire({
+                    title: 'Error',
+                    icon: 'error',
+                    text: 'Ha ocurrido un error'
+                });
             }
-        }
 
-        fetchData();
-    }, [plant]);
+            setPestData(response.data);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setObtainingData(false);
+        }
+    };
 
     return (
-        <>
-            <div>
-                <h1>API Data</h1>
-                <pre>{JSON.stringify(data, null, 2)}</pre>
+        <div className="container mx-auto p-4">
+            <h1 className="flex justify-center text-2xl font-bold mb-4">Información de Enfermedades y Plagas</h1>
+            <h1 className=" flex justify-center text-1xl font-semibold mb-4">Ingresa el Nombre de la planta</h1>
+
+            <div className="flex justify-center mb-4">
+                <input
+                    type="text"
+                    placeholder="Nombre de enfermedad o plaga"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className=" text-center w-80 p-2 rounded-lg mr-2"
+                />
+                <button onClick={getPestInfo} className="bg-blue-500 text-white p-2 rounded-lg">
+                    Obtener información
+                </button>
+            </div>
+
+            {obtainingData ? (
+                <p className='mt-40 flex justify-center items-center text-6xl font-serif font-bold'>Obteniendo datos...</p>
+            ) : (
                 <div>
-                    <input type="text" placeholder="Escribe el nombre de una planta"
-                        value={plant} onChange={handleInputChange}
-                    />
-                    <button>Buscar</button>
-                </div>
-                <div>
-                    {data && data.length > 0 && (
-                        <div>
-                            {data.map((item, index) => (
-                                <div key={index}>
-                                    <p>nombre: {item.common_name}</p>
-                                    <p>cientifico: {item.scientific_name}</p>
+                    {pestData && pestData.data && pestData.data.length > 0 ? (
+                        pestData.data.map((item, index) => (
+                            <div key={item.id} className="mb-4 p-4 bg-white rounded-md shadow-md">
+                                <h2 className="text-xl font-semibold mb-2">{item.common_name}</h2>
+                                <p className="text-lg">{item.scientific_name}</p>
+                                <div>
+                                    {item.description && item.description.length > 0 ? (
+                                        <div className="mt-4">
+                                            <h3 className="text-lg font-semibold mb-2">{item.description[0].subtitle}</h3>
+                                            <p>{item.description[0].description}</p>
+                                        </div>
+                                    ) : null}
                                 </div>
-                            ))}
-                        </div>
+                                <div>
+                                    {item.images && item.images.length > 0 ? (
+                                        <img
+                                            src={item.images[0].original_url}
+                                            alt={`Image 1`}
+                                            className="w-[50%] rounded-xl shadow-md mx-auto mt-4"
+                                        />
+                                    ) : null}
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <p className='mt-40 flex justify-center items-center text-6xl font-serif font-bold'>No se encontraron resultados.</p>
                     )}
                 </div>
-            </div>
-        </>
-    )
+            )}
+        </div>
+    );
 }
 
-export default Plants;
+export default Plant;
